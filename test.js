1e95dee74e7e657752182c3423168ef94d4d7814
razuAhmad <abdurrahmanrazu2000@gmail.com>
2023-08-01 16:01:30 +0600

    first commit

---
+++ test.js
@@ -0,0 +1,4 @@
+function showTextOnScreen(){
+    document.getElementById("demo1").innerHTML="This is demo 1 after clicking";
+    document.getElementById("demo2").innerHTML="This is demo 2 after clicking";
+}
